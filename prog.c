b8c3b7c6dfd93dbbd604a25d69e247c46ecf9e06
Portugaletz <37464348+Portugaletz@users.noreply.github.com>
2018-09-29 10:49:00 +0700

    BlessRNG this is fixed

--- prog.c
+++ prog.c
@@ -3,9 +3,9 @@
 
 int main(void) {
   float len;
-  scanf("%f", &(len));
+  scanf("%g", &(len));
   if (len>0){
-  printf("%g", 0.001*round(1000*len*len*sqrt(3))/4);
+  printf("%g", (len*len*sqrt(3))/4);
   return 0;
   }else{return 1;}
 }
