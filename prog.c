c9a9f7daaa495d80507ddf00c1ffa1f111bbf708
Portugaletz <37464348+Portugaletz@users.noreply.github.com>
2018-09-29 11:03:45 +0700

    Update prog.c

--- prog.c
+++ prog.c
@@ -5,7 +5,7 @@ int main(void) {
   float len;
   scanf("%f", &(len));
   if (len>0){
-  printf("%g", 0.001*round(1000*len*len*sqrt(3))/4);
+  printf("%g", 0.001*round((1000*len*len*sqrt(3))/4));
   return 0;
   }else{return 1;}
 }
