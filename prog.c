510225de98339dc45783e3f7aeba1432e436d4cf
Portugaletz <37464348+Portugaletz@users.noreply.github.com>
2018-09-29 10:36:53 +0700

    Fix
    roundf to round

--- prog.c
+++ prog.c
@@ -6,7 +6,7 @@ int main(void) {
   scanf("%f", &(len));
   if (len>0){
   //printf("%f\n", len);
-  printf("%g", 0.001*roundf(1000*len*len*sqrt(3))/4);
+  printf("%g", 0.001*round(1000*len*len*sqrt(3))/4);
   return 0;
   }else{return 1;}
 }
